b4c64ac5f746bac73dbcb078d0fe89ba35151dc1
Potatolad78 <rigby3006@gmail.com>
2021-05-16 10:45:22 -0700

    Early Animation and UI

--- Assets/LookatFollow.cs
+++ Assets/LookatFollow.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LookatFollow : MonoBehaviour
 {
     public Transform mTarget;
     float mSpeed = 3.0f;
     const float EPSILON= 4.0f;
+    public Animator anim;
+    float health = 100.0f;
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Animator>().enabled = true;
+        anim = GetComponent <Animator>();
+
     }
 
     // Update is called once per frame
@@ -18,15 +24,18 @@ public class LookatFollow : MonoBehaviour
     {
         transform.LookAt(mTarget.position);
         if((transform.position - mTarget.position).magnitude > EPSILON)
-        {    transform.Translate(0.0f, 0.0f, mSpeed*Time.deltaTime);
+        {   
+            transform.Translate(0.0f, 0.0f, mSpeed*Time.deltaTime);
             GetComponent<AudioSource>().UnPause();
             //GetComponent<Animator>().Stop(); 
-            GetComponent<Animator>().enabled = true; 
+            anim.SetBool("isMoving", true);
         }
         else
         {
+            health = health - 0.001f;
             GetComponent<AudioSource>().Pause();
-            GetComponent<Animator>().enabled = false; 
+            anim.SetBool("isMoving", false);
+            Debug.Log(health);
         }
     }
 }
