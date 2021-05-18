378e306e8c5de8883530592c74bc7d2dd76ca5cf
Potatolad78 <39474061+Potatolad78@users.noreply.github.com>
2021-05-18 09:54:58 -0700

    Menu and Asset and Save

--- Assets/SaveandPause.cs
+++ Assets/SaveandPause.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class SaveandPause : MonoBehaviour
 {
+    public GameObject Enemy;
+    public GameObject Menu;
     void Save()
     {
-        PrintStream FileOut = new PrintStream("Save.txt");
+        using(var writer = new StreamWriter(File.Open("Save.txt", FileMode.Create)))
+        {
+            writer.WriteLine(Enemy.transform.position.x);
+            writer.WriteLine(Enemy.transform.position.y);
+            writer.WriteLine(Enemy.transform.position.z);
+            writer.WriteLine(Enemy.transform.rotation.x);
+            writer.WriteLine(Enemy.transform.rotation.y);
+            writer.WriteLine(Enemy.transform.rotation.z);
+            writer.WriteLine(Enemy.transform.rotation.w);
+        }
 
+    }
+    void Load()
+    {
+       using(var reader = new StreamReader(File.Open("Save.txt", FileMode.Open)))
+       {
+            Enemy.transform.position = new Vector3(float.Parse(reader.ReadLine()),float.Parse(reader.ReadLine()),float.Parse(reader.ReadLine()));
+            Enemy.transform.rotation = new Quaternion(float.Parse(reader.ReadLine()),float.Parse(reader.ReadLine()),float.Parse(reader.ReadLine()),float.Parse(reader.ReadLine()));
+       } 
     }
 
     // Start is called before the first frame update
@@ -21,9 +41,17 @@ public class SaveandPause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene("Menu 3D");
+            Menu.SetActive(!Menu.activeSelf);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Save();
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            Load();
         }
     }
 }
