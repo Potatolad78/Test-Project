01bf60192a8790689f82e91b7ed8c88d1bfdb3c6
Potatolad78 <39474061+Potatolad78@users.noreply.github.com>
2021-05-20 09:49:45 -0700

    Jabbing

--- Assets/LookatFollow.cs
+++ Assets/LookatFollow.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System;
 
 public class LookatFollow : MonoBehaviour
 {
+    float timer = 15.0f;
+    float jabTimer = 1.0f;
     public Transform mTarget;
     float mSpeed = 3.0f;
-    const float EPSILON= 4.0f;
+    const float EPSILON= 2.0f;
     public Animator anim;
     float health = 100.0f;
     // Start is called before the first frame update
@@ -22,6 +25,32 @@ public class LookatFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(health);
+        timer -= Time.deltaTime;
+        Debug.Log(timer);
+        if(((transform.position - mTarget.position).magnitude < EPSILON ))
+        {
+        anim.SetBool("jabbingDistance", true);    
+        if (timer < 0 )
+            {
+            anim.SetBool("isJabbing", true);
+            jabTimer -= Time.deltaTime; 
+            if (jabTimer < 0)
+                {
+                    if((transform.position - mTarget.position).magnitude < EPSILON )
+                    {
+                        health = health - 5.0f;
+                    }
+                    timer = 15.0f;
+                    anim.SetBool("isJabbing", false);
+                    anim.SetBool("jabbingDistance", false);
+                }
+            }
+            if(jabTimer <= 0 && timer > 0)
+            {
+                jabTimer = 1.0f;
+            }
+        }
         transform.LookAt(mTarget.position);
         if((transform.position - mTarget.position).magnitude > EPSILON)
         {   
@@ -32,10 +61,8 @@ public class LookatFollow : MonoBehaviour
         }
         else
         {
-            health = health - 0.001f;
             GetComponent<AudioSource>().Pause();
             anim.SetBool("isMoving", false);
-            Debug.Log(health);
         }
     }
 }
